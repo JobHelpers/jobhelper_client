a61e642ea4566d468b4b054e2fac91dd351c2621
Ihor Byra <ihorbyra@gmail.com>
2022-06-14 23:14:10 +0300

    rm useless code

--- src/App.js
+++ src/App.js
@@ -1,29 +1,7 @@
-import { useEffect } from "react";
-
-// import {
-//   getCities,
-//   getSpecialities,
-//   getSubjects,
-//   getUniversities,
-//   getFaculties,
-//   getSpecialitiesWithGroupedSubjects,
-//   getSpecialitiesWithSubjects
-// } from './api';
 import FacultyFinder from './modules/FacultyFinder/FacultyFinder';
 import './assets/global-styles.css';
 
 function App() {
-
-  useEffect(() => {
-    // getCities().then((c) => console.log(c.data));
-    // getSpecialities().then((c) => console.log(c.data));
-    // getSpecialitiesWithSubjects().then((c) => console.log(c.data));
-    // getSpecialitiesWithGroupedSubjects().then((c) => console.log(c.data));
-    // getSubjects().then((c) => console.log(c.data));
-    // getUniversities({ speciality: 122, city: 1 }).then((c) => console.log(c.data));
-    // getFaculties({ university: 244, speciality: 122 }).then((c) => console.log(c.data));
-  }, []);
-
   return (
     <FacultyFinder />
   );
