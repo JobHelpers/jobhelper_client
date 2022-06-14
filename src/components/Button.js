dad3f3e39ecd77f952e5b69e61a0893fed7d5fcc
Ihor Byra <ihorbyra@gmail.com>
2022-06-14 22:29:17 +0300

    rm useless files

--- src/components/Button.js
+++
@@ -1,8 +0,0 @@
-import './style.css';
-
-export const Button = ({text, setText}) => {
-  console.log(text);
-  return (
-    <div onClick={() => setText('New description')} className="button">Submit</div>
-  );
-}
